9d755b4024106db95b5be07950bb8f24b8031540
KEAN CHANTHY <kean.chanthy.1222@rupp.edu.kh>
2025-10-03 13:52:31 +0700

    fix commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { CartProvider } from "./context/CartContext";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
