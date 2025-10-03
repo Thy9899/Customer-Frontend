404fadf58c85435470faaca11f9154524680bd4e
KEAN CHANTHY <kean.chanthy.1222@rupp.edu.kh>
2025-10-03 11:07:29 +0700

    update first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Customer-Frontend">
       <CartProvider>
         <App />
       </CartProvider>
